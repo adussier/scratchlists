48f4d7428d96072fb07b38aae1f0434b7b73d40c
Amel Dussier <amel.dussier@gmail.com>
2020-06-09 17:32:31 +0200

    Landing page

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Config from "./config.js"
+import './Landing.css';
 
 export default function Landing(props) {
     return (
-        <div>
-            <h1>Scratchlists</h1>
-            <a href={Config.COGNITO_UI_URL}>Sign in or sign up</a>
+        <div className="bg">
+            <div className="container pt-5">
+                <div>
+                    <h1 className="display-1 text-dark text-center">
+                        <b><i>ScratchLists</i></b>
+                    </h1>
+                </div>
+                <div className="pb-5">
+                    <p className="lead text-dark text-center">
+                        <b><i>Your todo lists in the Cloud</i></b>
+                    </p>
+                </div>
+                <div className="py-5">
+                    <p className="text-center">
+                        <a href={Config.COGNITO_UI_URL} className="btn btn-lg btn-outline-success">Sign in or sign up</a>
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
